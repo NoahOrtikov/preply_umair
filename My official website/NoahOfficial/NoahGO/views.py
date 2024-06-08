d4afa93059c1e887f6a323110b7c1e998a299771
noah <noahortikov@gmail.com>
2024-06-08 10:39:31 -0500

    making bttrfly game

--- My official website/NoahOfficial/NoahGO/views.py
+++ My official website/NoahOfficial/NoahGO/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,'index.html')
 
